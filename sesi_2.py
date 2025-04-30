e0972238e0046ba4c4452041458ef24fba94f87a
KruGong356 <ghiffary.abdul.hakim01@gmail.com>
2025-04-30 12:38:05 +0700

    learn if and for

---
+++ sesi_2.py
@@ -0,0 +1,20 @@
+usia = 20
+
+if usia < 13:
+    print(f'usia {usia} adalah anak-anak')
+elif usia >= 13 and usia <= 20:
+    print(f'usia {usia} adalah remaja') 
+elif usia >= 21 and usia <= 40:
+    print(f'usia {usia} adalah dewasa')
+else:
+    print(f'usia {usia} adalah tua')
+    
+    
+for i in range(6):
+    print(f'{i}')
+    
+for i in range(2, 11):
+    print(f"{i}")
+    
+for i in range(15,1,-2):
+    print(f'{i}')
